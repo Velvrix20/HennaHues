60843505db4d5f61410936e21cc3283202549475
Velvrix20 <velvrix@proton.me>
2025-05-22 20:44:13 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -9,11 +9,6 @@ import { Toaster } from "react-hot-toast";
 import Link from "next/link";
 
 export const MyContext = React.createContext();
-export const metadata = {
-  metadataBase: new URL('https://hennahues.velvrix.site'), // Your production URL
-  title: 'Henna Hues',
-  // ...other metadata
-}
 export default function Home() {
   const [photosArray, setPhotosArray] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
@@ -118,4 +113,10 @@ export default function Home() {
       </main>
     </MyContext.Provider>
   )
+}
+
+export const metadata = {
+  metadataBase: new URL('https://hennahues.com'), // Your production URL
+  title: 'Henna Hues',
+  // ...other metadata
 }
