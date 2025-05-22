1c9e52e738d918079aca37d72c4a445025707304
Velvrix20 <velvrix@proton.me>
2025-05-22 16:50:33 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+/**const nextConfig = { This is default 
     images: {
         domains: ["images.pexels.com"],
     },
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig; */  
+
+module.export: {
+domains: [
+    'https://bjulguxvkirbdsrcygfy.supabase.co'
+   ],
+  },
+}
